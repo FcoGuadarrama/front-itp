6e20eabadc310a8fc91dcb78859b5db7d20a13d4
Francisco Guadarrama <ingfranciscoguadarrama@gmail.com>
2022-04-08 01:04:16 -0500

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavBarComponent } from './components/shared/nav-bar/nav-bar.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavBarComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        ReactiveFormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+
+  public rows: any = [{
+    id: Number,
+    activity: String,
+  }];
+
   constructor() { }
 
+  form = new FormGroup({});
+
   ngOnInit(): void {
+    this.rows = [
+      {
+        id: 1,
+        activity: 'Diseñar programas de computación y explorar nuevas aplicaciones tecnológicas para uso del internet',
+      },
+      {
+        id: 2,
+        activity: 'Criar, cuidar y tratar aninamles domésticos y de campo',
+      },
+      {
+        id: 3,
+        activity: 'Investigar sobre áreas verdes, medio ambiente y cambios climáticos',
+      },
+      {
+        id: 4,
+        activity: 'Ilustrar, dibujar y animar digitalmente',
+      },
+      {
+        id: 5,
+        activity: 'Seleccionar, capacitar y motivar al personal de una organización/empresa',
+      },
+      {
+        id: 6,
+        activity: 'Realizar excavaciones para descubrir restos del pasado',
+      },
+      {
+        id: 7,
+        activity: 'Resolver problemas de cálculo para construir un puente',
+      },
+      {
+        id: 8,
+        activity: 'Diseñar cursos para enseñar a la gente sobre temas de salud e higiene',
+      },
+      {
+        id: 9,
+        activity: 'Tocar un instrumento y componer música',
+      },
+      {
+        id: 10,
+        activity: 'Planificar cuáles son las metas deuna organización pública o privada a mediano y largo plazo',
+      }
+    ];
+    for (let i = 1; i <= this.rows.length; i++){
+      this.form.addControl(i.toString(), new FormControl('', Validators.required))
+    }
+  }
+
+  print(){
+    console.log(this.form.value);
   }
 
 }
